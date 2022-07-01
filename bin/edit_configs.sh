d679488991ab2e77a2af3b4532d73ee1d0c8881a
niveK77pur <kevinbiewesch@yahoo.fr>
2022-07-02 00:14:51 +0200

    update script to pick configs from this repo

--- bin/edit_configs.sh
+++ bin/edit_configs.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-directories=(
-    alacritty/
-    i3/
-    lf/
-    bash/
-    qtile/
-)
+# directories=(
+#     alacritty/
+#     i3/
+#     lf/
+#     bash/
+#     qtile/
+# )
+# 
+# 
+# cd $HOME/.config
+# config_file="$(find ${directories[@]} -type f | dmenu -i -l 10 -p "config files: ")"
 
-
-cd $HOME/.config
-config_file="$(find ${directories[@]} -type f | dmenu -i -l 10 -p "config files: ")"
-
+cd "$HOME/.config/77configs/config"
+config_file="$(find -type f | dmenu -i -l 10 -p "config files: ")"
 [ -n "$config_file" ] && $TERMINAL --command nvim "$PWD/$config_file" &
