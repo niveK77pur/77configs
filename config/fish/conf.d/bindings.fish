4c94b16657017a718f44576a9bfd74720437f3b3
niveK77pur <kevinbiewesch@yahoo.fr>
2023-06-18 21:27:08 +0200

    fish: use suggested lfcd binding

--- config/fish/conf.d/bindings.fish
+++ config/fish/conf.d/bindings.fish
@@ -1,4 +1,4 @@
-bind \co lfcd
+bind \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
 
 if [ "$fish_key_bindings" = fish_vi_key_bindings ];
     bind -Minsert ! __history_previous_command
