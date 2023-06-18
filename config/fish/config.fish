b7f03d513ae9c941459de53ed02beac6a045ebf0
niveK77pur <kevinbiewesch@yahoo.fr>
2023-06-18 21:19:07 +0200

    fish: improve sections in config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,28 +3,30 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                   Set values
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Hide welcome message
-set fish_greeting
-set VIRTUAL_ENV_DISABLE_PROMPT "1"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
-# https://github.com/fish-shell/fish-shell/issues/1819#issuecomment-63728658
-set -p fish_function_path (find ~/.config/77configs/config/fish/functions -mindepth 1 -type d)
-# set -p __fish_config_dir (find ~/.config/77configs/config/fish/conf.d -mindepth 1 -type d)
-
-## Export variable need for qt-theme
-if type "qtile" >> /dev/null 2>&1
-    set -x QT_QPA_PLATFORMTHEME "qt5ct"
-end
-
-# Set settings for https://github.com/franciscolourenco/done
-set -U __done_min_cmd_duration 10000
-set -U __done_notification_urgency_level low
-
+# Shell variables --------------------------------------------------------------
 set -gx EDITOR nvim
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 set -gx LYEDITOR "nvr +:'call cursor(%(line)s,%(char)s)' %(file)s"
+
+# Hide welcome message ---------------------------------------------------------
+set fish_greeting
+# set VIRTUAL_ENV_DISABLE_PROMPT "1"
+
+# Fake recursive function path -------------------------------------------------
+# https://github.com/fish-shell/fish-shell/issues/1819#issuecomment-63728658
+set -p fish_function_path (find ~/.config/77configs/config/fish/functions -mindepth 1 -type d)
+# set -p __fish_config_dir (find ~/.config/77configs/config/fish/conf.d -mindepth 1 -type d)
+
+# Export variable need for qt-theme --------------------------------------------
+if type "qtile" >> /dev/null 2>&1
+    set -x QT_QPA_PLATFORMTHEME "qt5ct"
+end
+
+# Set settings for https://github.com/franciscolourenco/done -------------------
+set -U __done_min_cmd_duration 10000
+set -U __done_notification_urgency_level low
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                               Environment setup
@@ -34,6 +36,8 @@ set -gx LYEDITOR "nvr +:'call cursor(%(line)s,%(char)s)' %(file)s"
 if test -f ~/.fish_profile
     source ~/.fish_profile
 end
+
+# Extend PATH ------------------------------------------------------------------
 
 # Add ~/.local/bin to PATH
 if test -d ~/.local/bin
