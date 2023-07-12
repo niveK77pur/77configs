9b2c826151736ac1ae2562a0982c540ab6b9a56b
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-07-12 13:47:59 +0200

    fish: ls does not show long list

--- config/fish/functions/alias/renew_commands/ls.fish
+++ config/fish/functions/alias/renew_commands/ls.fish
@@ -1,7 +1,7 @@
 # Defined via `source`
 function ls \
-    --wraps='exa -l --color=always --group-directories-first --icons' \
-    --description 'alias ls=exa -l --color=always --group-directories-first --icons'
-  exa -l --color=always --group-directories-first --icons $argv
+    --wraps='exa --color=always --group-directories-first --icons' \
+    --description 'alias ls=exa --color=always --group-directories-first --icons'
+  exa --color=always --group-directories-first --icons $argv
         
 end
