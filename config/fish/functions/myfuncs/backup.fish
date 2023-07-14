ba512af05518522488ed13a21d80dde72c6040ad
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-07-14 10:44:48 +0200

    fish: backup function allows backing up directories

--- config/fish/functions/myfuncs/backup.fish
+++ config/fish/functions/myfuncs/backup.fish
@@ -1,4 +1,4 @@
 # Defined in /home/kevinb/.config/fish/config.fish @ line 81
 function backup --argument filename
-    cp $filename $filename.(date +%F-%T).bak
+    cp -r $filename $filename.(date +%F-%T).bak
 end
