06533c6b63ea8ac39958bb229fdc38ab22ae0962
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-01-03 19:14:49 +0100

    mount.go: refactor conversion from []Device to/from []array

--- compiled-programs/mount/internal/environment/environment.go
+++ compiled-programs/mount/internal/environment/environment.go
@@ -53,11 +53,7 @@ func (env evironment) Choose(devices []dev.Device) dev.Device {
 }
 
 func (env evironment) choose_terminal(devices []dev.Device) dev.Device {
-	options := make([]string, len(devices))
-	for i, device := range devices {
-		options[i] = device.As_string()
-	}
-
+	options := devices_to_strings(&devices)
 	selection, err := pterm.DefaultInteractiveSelect.
 		WithDefaultText("Choose a device").
 		WithOptions(options).
@@ -66,26 +62,51 @@ func (env evironment) choose_terminal(devices []dev.Device) dev.Device {
 		slog.Error("Error while choosing device", "err", err.Error())
 	}
 
+	device := get_device_from_string_list(selection, devices, options)
+	return device
+}
+
+func devices_to_strings(devices *[]dev.Device) []string {
+	options := make([]string, len(*devices))
+	for i, device := range *devices {
+		options[i] = device.As_string()
+	}
+	return options
+}
+
+func get_device_from_string_list(
+	device_string string,
+	devices []dev.Device,
+	devices_string_list []string,
+) dev.Device {
 	var idx int = -1
-	for i, option := range options {
-		if option == selection {
+	for i, option := range devices_string_list {
+		if option == device_string {
 			idx = i
 			break
 		}
 	}
+
 	if idx < 0 {
 		// This case should not occur
 		slog.Error(
 			"Failed to find selected string in array",
-			"selection",
-			selection,
-			"options",
-			options,
+			"device string",
+			device_string,
+			"device list",
+			devices_string_list,
 		)
 		panic("Failed to find selected string in array")
 	}
-
-	slog.Debug("Selected device", "device string", selection, "index", idx, "device", devices[idx])
+	slog.Debug(
+		"Selected device",
+		"device string",
+		device_string,
+		"index",
+		idx,
+		"device",
+		devices[idx],
+	)
 
 	return devices[idx]
 }
