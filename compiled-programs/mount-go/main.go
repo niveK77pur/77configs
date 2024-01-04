abf8caba7522abffbe658cdd5a59c7cfb64d046b
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-01-04 18:06:45 +0100

    mount.go: globally specify environment to be used and worked in

--- compiled-programs/mount-go/main.go
+++ compiled-programs/mount-go/main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	dev "niveK77pur/mount-go/internal/device"
-	env "niveK77pur/mount-go/internal/environment"
+	"niveK77pur/mount-go/internal/environment"
 
 	"github.com/pterm/pterm"
 )
+
+var env = environment.TERMINAL
 
 func main() {
 	logger := slog.New(pterm.NewSlogHandler(&pterm.DefaultLogger))
 	// pterm.DefaultLogger.Level = pterm.LogLevelDebug
 	slog.SetDefault(logger)
 
-	env.X11.Notify("Hello")
 	devices, err := dev.GetDevices()
 	if err != nil {
 		slog.Error(err.Error())
 		return
 	}
-	device, err := env.TERMINAL.Choose(devices)
+	device, err := env.Choose(devices)
 	if err != nil {
 		slog.Error(err.Error())
 		return
 	}
-	_, err = device.Mount()
-	device.Unmount()
-	// env.X11.Choose(devices)
+	if device.Is_mounted() {
+		if err := device.Unmount(); err != nil {
+			env.Notify(
+				fmt.Sprintf("Error occured while unmounting %s:\n%s", device.Path, err.Error()),
+			)
+		} else {
+			env.Notify(fmt.Sprintf("Unmounted %s", device.Path))
+		}
+	} else {
+		path, err := device.Mount()
+		if err != nil {
+			env.Notify(fmt.Sprintf("Error occured while mounting %s:\n%s", device.Path, err.Error()))
+		} else {
+			env.Notify(fmt.Sprintf("Mounted %s at %s", device.Path, path))
+		}
+	}
 }
