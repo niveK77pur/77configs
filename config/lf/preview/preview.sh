ca077513de29d0181a9d50aa59cfec572d85d6e6
niveK77pur <kevinbiewesch@yahoo.fr>
2024-03-16 15:32:16 +0100

    lf: apply shfmt
    style(lf_sixel_preview): apply shfmt formatter

--- config/lf/preview/preview.sh
+++ config/lf/preview/preview.sh
@@ -5,14 +5,14 @@ file=$1
 filetype="$(file -Lb --mime-type "$file")"
 script_path_prefix=~/.config/77configs/config/lf/preview
 
-if [[ "$filetype" =~ ^image ]]; then
-    if type chafa > /dev/null && [[ $(lf --version) -gt 30 ]]; then
+if [[ $filetype =~ ^image ]]; then
+    if type chafa >/dev/null && [[ $(lf --version) -gt 30 ]]; then
         "$script_path_prefix/lf_sixel_preview" "$@"
     else
         file --brief "$file"
     fi
-elif [[ "$filetype" =~ ^video ]]; then
-    if type chafa > /dev/null && [[ $(lf --version) -gt 30 ]]; then
+elif [[ $filetype =~ ^video ]]; then
+    if type chafa >/dev/null && [[ $(lf --version) -gt 30 ]]; then
         w="$2"
         h="$3"
         ffmpegthumbnailer -s0 -m -f -i "$file" -c jpeg -o- | chafa -f sixel -s "${w}x${h}"
@@ -20,7 +20,7 @@ elif [[ "$filetype" =~ ^video ]]; then
         file --brief "$file"
     fi
 else
-    if type pistol > /dev/null; then
+    if type pistol >/dev/null; then
         pistol "$file"
     else
         "$script_path_prefix/lf_preview_custom.sh" "$@"
