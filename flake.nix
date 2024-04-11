1e85dc6cdf44bb0ba0ca4a7ca2450ed6410f4b78
niveK77pur <kevinbiewesch@yahoo.fr>
2024-04-12 00:42:08 +0200

    home-manager: Put all configs under dedicated folder

--- flake.nix
+++ flake.nix
@@ -23,7 +23,9 @@
 
       # Specify your home configuration modules here, for example,
       # the path to your home.nix.
-      modules = [./home.nix];
+      modules = [
+        ./nix/home.nix
+      ];
 
       # Optionally use extraSpecialArgs
       # to pass through arguments to home.nix
