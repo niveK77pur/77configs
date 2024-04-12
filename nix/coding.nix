bb43bc48359568661a32ff0c7b98452352bd72d4
niveK77pur <kevinbiewesch@yahoo.fr>
2024-04-12 20:30:03 +0200

    home-manager: Add coding packages

--- nix/coding.nix
+++ nix/coding.nix
@@ -5,8 +5,27 @@
   ...
 }: {
   config = {
-    home.packages = [
+    home.packages = with pkgs; [
       alejandra.defaultPackage.${system}
     ];
+
+    programs.lazygit.enable = true;
+    programs.ripgrep.enable = true;
+
+    programs.git = {
+      enable = true;
+      userEmail = "kevinbiewesch@yahoo.fr";
+      userName = "Kevin Laurent Biewesch";
+      # delta.enable = true;
+      diff-so-fancy.enable = true;
+      # difftastic.enable = true;
+    };
+
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      # withNodeJs = true;
+      # withPython3 = true;
+    };
   };
 }
