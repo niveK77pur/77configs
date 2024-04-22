1232f579fb7b53cab8062b9d27f4998ad30e2735
niveK77pur <kevinbiewesch@yahoo.fr>
2024-04-22 16:06:21 +0200

    fish(home-manager): remove broken `!` and `$` bindings

--- nix/terminal/fish.nix
+++ nix/terminal/fish.nix
@@ -24,14 +24,6 @@
       shellInit = ''
         # lfcd
         bind \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
-
-        if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-            bind -Minsert ! __history_previous_command
-            bind -Minsert '$' __history_previous_command_arguments
-        else
-            bind ! __history_previous_command
-            bind '$' __history_previous_command_arguments
-        end
       '';
 
       functions = lib.mkMerge [
