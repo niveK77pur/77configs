314511683dd0a2189909d666b559463eebdf2d73
niveK77pur <kevinbiewesch@yahoo.fr>
2024-05-03 23:20:23 +0200

    home-manager(pistol): refactor to not include dependencies as home.packages

--- nix/terminal/pistol.nix
+++ nix/terminal/pistol.nix
@@ -1,28 +1,27 @@
 {pkgs, ...}: {
   config = {
-    home.packages = with pkgs; [
-      python312Packages.pygments
-      poppler_utils
-    ];
-
     programs.pistol = {
       enable = true;
-      associations = [
+      associations = let
+        pygmentize = "${pkgs.python312Packages.pygments}/bin/pygmentize";
+        pdftotext = "${pkgs.poppler_utils}/bin/pdftotext";
+        pdfinfo = "${pkgs.poppler_utils}/bin/pdfinfo";
+      in [
         {
           fpath = ".*/aur[^/]*/.*SRCINFO$";
-          command = "pygmentize -O style=trac -l srcinfo %pistol-filename%";
+          command = "${pygmentize} -O style=trac -l srcinfo %pistol-filename%";
         }
         {
           fpath = ".*/aur[^/]*/.*PKGBUILD$";
-          command = "pygmentize -O style=trac -l bash %pistol-filename%";
+          command = "${pygmentize} -O style=trac -l bash %pistol-filename%";
         }
         {
           mime = "application/pdf";
-          command = "pdftotext %pistol-filename% -";
+          command = "${pdftotext} %pistol-filename% -";
         }
         {
           mime = "application/pdf";
-          command = "pdfinfo %pistol-filename%";
+          command = "${pdfinfo} %pistol-filename%";
         }
       ];
     };
