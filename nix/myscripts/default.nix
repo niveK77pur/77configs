92bb70d82837cfcffaf02cd8c568e8beb56a9fdd
niveK77pur <kevinbiewesch@yahoo.fr>
2024-05-04 22:34:36 +0200

    home-manager(we): fix wrong version of MPV being used
    The mpv version was not the one created by home-manager, instead it used
    the default one from pkgs. The 4k upscaling only worked because it
    happened to be present in the `~/.config/mpv` directory. All other mpv
    scripts/plugins are not present in that user config directory, hence did
    seemingly not take any effect.

--- nix/myscripts/default.nix
+++ nix/myscripts/default.nix
@@ -1,7 +1,11 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   config = {
     home.packages = [
-      (pkgs.callPackage ./we.nix {})
+      (pkgs.callPackage ./we.nix {mpv = config.programs.mpv.finalPackage;})
       (pkgs.callPackage ./ccopy.nix {})
       (pkgs.callPackage ./mount.nix {})
       # (pkgs.callPackage ./mount-go.nix {})
