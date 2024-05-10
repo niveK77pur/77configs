2207f800ae1f59ef1f42acea6c8b491872e3102a
niveK77pur <kevinbiewesch@yahoo.fr>
2024-05-10 17:14:38 +0200

    home-manager(neovim): Add gcc to avoid errors from treesitter
    On some filetypes, an error would pop up saying that no C compiler is
    available. This is most likely related to treesitter not being able to
    grab its parsers.

--- nix/coding/neovim.nix
+++ nix/coding/neovim.nix
@@ -1,8 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   config = {
     programs.neovim = {
       enable = true;
       defaultEditor = true;
+      extraPackages = [
+        pkgs.gcc # mostly for tree-sitter
+      ];
     };
   };
 }
