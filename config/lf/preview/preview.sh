ca2503c5a3c360d1e98e09bb04e3c59e37525490
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-05-15 14:12:12 +0200

    lf(preview.sh): Refactor checking of lf version

--- config/lf/preview/preview.sh
+++ config/lf/preview/preview.sh
@@ -4,15 +4,19 @@ file=$1
 
 filetype="$(file -Lb --mime-type "$file")"
 script_path_prefix=~/.config/77configs/config/lf/preview
+valid_lf_version=$(
+    [[ $(lf --version | sed 's/r//g') -gt 30 ]]
+    echo $?
+)
 
 if [[ $filetype =~ ^image ]]; then
-    if type chafa >/dev/null && [[ $(lf --version | sed 's/r//g') -gt 30 ]]; then
+    if type chafa >/dev/null && [ "$valid_lf_version" -eq 0 ]; then
         "$script_path_prefix/lf_sixel_preview" "$@"
     else
         file --brief "$file"
     fi
 elif [[ $filetype =~ ^video ]]; then
-    if type chafa >/dev/null && [[ $(lf --version | sed 's/r//g') -gt 30 ]]; then
+    if type chafa >/dev/null && [ "$valid_lf_version" -eq 0 ]; then
         w="$2"
         h="$3"
         ffmpegthumbnailer -s0 -m -f -i "$file" -c jpeg -o- | chafa -f sixel -s "${w}x${h}" --animate off --polite on
