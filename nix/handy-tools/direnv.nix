9472fa52d5649e04c137df44ce0dfca9fe20e727
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-05-22 12:45:05 +0200

    home-manager(direnv): Add option to hide_env_diff
    Especially with nix flakes, the export log is quite large, and most of
    it is not very interesting.

--- nix/handy-tools/direnv.nix
+++ nix/handy-tools/direnv.nix
@@ -3,6 +3,11 @@
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
+      config = {
+        global = {
+          hide_env_diff = true;
+        };
+      };
     };
   };
 }
