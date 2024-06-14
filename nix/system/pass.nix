00a9a2d72223ac6cf224ce1c2028efe32937c01c
niveK77pur <kevinbiewesch@yahoo.fr>
2024-06-15 01:40:01 +0200

    refactor(pass.nix): Address statix warnings

--- nix/system/pass.nix
+++ nix/system/pass.nix
@@ -1,19 +1,21 @@
 {pkgs, ...}: {
   config = {
-    programs.gpg.enable = true;
+    programs = {
+      gpg.enable = true;
+      password-store = {
+        enable = true;
+        settings = {
+          PASSWORD_STORE_DIR = "$HOME/.password-store";
+        };
+      };
+      mr.settings = {
+        ".password-store" = {
+          checkout = "git clone git@github.com:niveK77pur/super-octo-succotash.git .password-store";
+        };
+      };
+    };
     services.gpg-agent.enable = true;
     services.gpg-agent.pinentryPackage = pkgs.pinentry-all;
     # home.packages = [pkgs.pinentry-all];
-    programs.password-store = {
-      enable = true;
-      settings = {
-        PASSWORD_STORE_DIR = "$HOME/.password-store";
-      };
-    };
-    programs.mr.settings = {
-      ".password-store" = {
-        checkout = "git clone git@github.com:niveK77pur/super-octo-succotash.git .password-store";
-      };
-    };
   };
 }
