be22d3afacd27dd8dbbba0c9ab9c52247053ebce
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-07-28 15:19:02 +0200

    feat(browsing/firefox.nix): Ability to disable

--- nix/browsing/firefox.nix
+++ nix/browsing/firefox.nix
@@ -1,5 +1,11 @@
-{...}: {
-  config = {
+{
+  lib,
+  config,
+  ...
+}: {
+  options.firefox.enable = lib.mkEnableOption "firefox" // {default = true;};
+
+  config = lib.mkIf config.firefox.enable {
     programs.firefox.enable = true;
   };
 }
