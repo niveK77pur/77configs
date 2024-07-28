28cdc168bd413d19a39d6d79250824e593b7f199
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-07-28 15:19:23 +0200

    feat(browsing/vieb.nix): Ability to disable

--- nix/browsing/vieb.nix
+++ nix/browsing/vieb.nix
@@ -1,5 +1,12 @@
-{pkgs, ...}: {
-  config = {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options.vieb.enable = lib.mkEnableOption "vieb" // {default = true;};
+
+  config = lib.mkIf config.vieb.enable {
     home.packages = [pkgs.vieb];
     xdg.configFile.Vieb = {
       source = ../../config/Vieb;
