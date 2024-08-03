cd1e811dcc45a5d17db1a9aea0f9c33d37f36072
niveK77pur <kevinbiewesch@yahoo.fr>
2024-08-03 16:45:13 +0200

    feat(lutris.nix): Add packages to lutris installation

--- nix/gaming/lutris.nix
+++ nix/gaming/lutris.nix
@@ -1,5 +1,15 @@
 {pkgs, ...}: {
   config = {
-    home.packages = with pkgs; [lutris];
+    home.packages = with pkgs; [
+      (lutris.override {
+        extraLibraries = pkgs: [
+          wineWowPackages.stable
+          wineWowPackages.staging
+          winetricks
+          keyutils
+          mangohud
+        ];
+      })
+    ];
   };
 }
