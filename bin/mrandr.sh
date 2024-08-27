6aaf00d7f3c330a9df2ac499d183b39e31a06acc
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-08-27 22:05:14 +0200

    feat(mrandr.sh): Pass displays as arguments + apply formatter

--- bin/mrandr.sh
+++ bin/mrandr.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-OUTPUT=HDMI1
-SCREEN=eDP1
+OUTPUT=${1:-HDMI1}
+SCREEN=${2:-eDP1}
 
 position=$(echo "above below same-as left-of right-of off" | tr ' ' '\n' | dmenu -i -l 10 -p "Screen position")
 case "$position" in
-    "")     ;;
-    off)    xrandr --output "$OUTPUT" --off ;;
-    *)      xrandr --output "$OUTPUT" --auto "--$position" "$SCREEN" ;;
+"") ;;
+off) xrandr --output "$OUTPUT" --off ;;
+*) xrandr --output "$OUTPUT" --auto "--$position" "$SCREEN" ;;
 esac
