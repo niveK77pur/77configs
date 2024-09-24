7a43c3161ff7482b3a08c77b7c1b422d91027b15
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-09-24 07:41:08 +0000

    feat(fonts)!: Ability to enable fonts category
    BREAKING CHANGE: Disabled by default

--- nix/fonts/default.nix
+++ nix/fonts/default.nix
@@ -1,5 +1,11 @@
-{pkgs, ...}: {
-  config = {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  options.fonts.enableAll = lib.mkEnableOption "fonts";
+  config = lib.mkIf config.fonts.enableAll {
     fonts.fontconfig.enable = true;
 
     home.packages = with pkgs; [
