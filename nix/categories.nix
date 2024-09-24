8eb25633e74b15197a4da97271757b57c4fac57c
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2024-09-24 08:22:11 +0000

    feat(categories.nix): Ability to enable every category

---
+++ nix/categories.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  imports = [
+    ./browsing
+    ./coding
+    ./fonts
+    ./gaming
+    ./handy-tools
+    ./media
+    ./messaging
+    ./myscripts
+    ./system
+    ./terminal
+  ];
+
+  options.categories.enableAll = lib.mkEnableOption "categories";
+
+  config = lib.mkIf config.categories.enableAll {
+    browsing.enableAll = lib.mkDefault true;
+    coding.enableAll = lib.mkDefault true;
+    fonts.enableAll = lib.mkDefault true;
+    gaming.enableAll = lib.mkDefault true;
+    handy-tools.enableAll = lib.mkDefault true;
+    media.enableAll = lib.mkDefault true;
+    messaging.enableAll = lib.mkDefault true;
+    myscripts.enableAll = lib.mkDefault true;
+    system.enableAll = lib.mkDefault true;
+    terminal.enableAll = lib.mkDefault true;
+  };
+}
