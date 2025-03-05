bcbcb21e9268f146c4efa3495b628d3eba56c865
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-03-05 15:05:40 +0100

    feat(hyprland): Prefer system installed Hyprland
    On non-NixOS systems, the home-manager provided Hyprland may cause
    problems. Thus, we shall prefer installing the window manager using from
    the system instead of home-manager.

--- nix/window-managers/hyprland.nix
+++ nix/window-managers/hyprland.nix
@@ -11,7 +11,12 @@ in {
 
   config = lib.mkIf cfg.enable {
     programs.kitty.enable = true; # required for the default Hyprland config
-    wayland.windowManager.hyprland.enable = true; # enable Hyprland
+    wayland.windowManager.hyprland = {
+      enable = true; # enable Hyprland
+      # but do not install Hyprland
+      package = null;
+      portalPackage = null;
+    };
 
     # Optional, hint Electron apps to use Wayland:
     # home.sessionVariables.NIXOS_OZONE_WL = "1";
