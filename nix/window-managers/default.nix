0fba411d86d0f1ce3cd3a1a06b1c58dba65344dd
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-03-06 19:45:03 +0100

    feat(dunst): Add to home-manager

--- nix/window-managers/default.nix
+++ nix/window-managers/default.nix
@@ -6,6 +6,7 @@
   imports = [
     ./hyprland.nix
     ./fuzzel.nix
+    ./dunst.nix
   ];
 
   options.window-managers.enableAll = lib.mkEnableOption "window-managers";
@@ -13,5 +14,6 @@
   config = lib.mkIf config.window-managers.enableAll {
     hyprland.enable = lib.mkDefault true;
     fuzzel.enable = lib.mkDefault true;
+    dunst.enable = lib.mkDefault true;
   };
 }
