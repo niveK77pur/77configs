dfca4e959db06cb76f6cb2e530856286262390a7
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-03-06 17:43:49 +0100

    feat(fuzzel): Add fuzzel as a Wayland application launcher

--- nix/window-managers/default.nix
+++ nix/window-managers/default.nix
@@ -5,11 +5,13 @@
 }: {
   imports = [
     ./hyprland.nix
+    ./fuzzel.nix
   ];
 
   options.window-managers.enableAll = lib.mkEnableOption "window-managers";
 
   config = lib.mkIf config.window-managers.enableAll {
     hyprland.enable = lib.mkDefault true;
+    fuzzel.enable = lib.mkDefault true;
   };
 }
