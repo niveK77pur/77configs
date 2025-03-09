60ed71e3c9c3b99c75be8d7ce625936a8d69700a
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-03-09 11:46:00 +0100

    feat(hyprland): Remove env vars that did not seem to have any effect
    It seems that only the `NIXOS_OZONE_WL` variable helped in making
    Discord and alike not blurry and low resolution anymore. The rest shall
    be commented in case they might prove useful in the future, but for now,
    the clutter shall be removed.

--- nix/window-managers/hyprland.nix
+++ nix/window-managers/hyprland.nix
@@ -7,17 +7,17 @@
   envVars = {
     # Hint Electron apps to use Wayland:
     NIXOS_OZONE_WL = "1";
-    XCURSOR_SIZE = "24";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    GDK_BACKEND = "wayland,x11";
-    QT_QPA_PLATFORM = "wayland;xcb";
-    SDL_VIDEODRIVER = "wayland";
-    CLUTTER_BACKEND = "wayland";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
+    # XCURSOR_SIZE = "24";
+    # XDG_CURRENT_DESKTOP = "Hyprland";
+    # XDG_SESSION_TYPE = "wayland";
+    # XDG_SESSION_DESKTOP = "Hyprland";
+    # GDK_BACKEND = "wayland,x11";
+    # QT_QPA_PLATFORM = "wayland;xcb";
+    # SDL_VIDEODRIVER = "wayland";
+    # CLUTTER_BACKEND = "wayland";
+    # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    # QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    # QT_QPA_PLATFORMTHEME = "qt5ct";
   };
   mkSubMap = {
     name,
