5ea99ca9f95c863e17a90ee39a086350bfe66cbc
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-03-16 21:04:26 +0100

    feat(flake): Users are created using a custom function
    There is quite some boiler plate that is effectively being repeated for
    each user. This adds noise and also makes it less simple to maintain a
    consistent basis across all user profiles.
    
    The custom function here shall abstract the boilerplate code away from
    the actual definition, so that one can focus more clearly on the user's
    modules instead.

--- flake.nix
+++ flake.nix
@@ -36,104 +36,87 @@
       userEmail = "kevinbiewesch@yahoo.fr";
       userName = "Kevin Laurent Biewesch";
     };
-  in {
-    homeConfigurations = {
-      "kevin" = home-manager.lib.homeManagerConfiguration {
+    #  {{{
+    makeUser = user: modules: {
+      "${user}" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [
-          nix-index-database.hmModules.nix-index
-          ./nix/home.nix
-          ./nix/categories.nix
-          {
-            config = {
-              categories.enableAll = true;
-              mpv.withAnime4k = true;
-              wezterm.overrides = {
-                window_background_opacity = 0.97;
-                font_size = 11.1;
-              };
-              git = {inherit (git) userName userEmail;};
-              jj = {inherit (git) userName userEmail;};
-              myscripts.mrandr = {
-                SCREEN = "eDP-2";
-                OUTPUT = "DP-1";
-              };
-            };
-          }
-        ];
+        modules =
+          [
+            nix-index-database.hmModules.nix-index
+            ./nix/home.nix
+            ./nix/categories.nix
+          ]
+          ++ modules;
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
         extraSpecialArgs = {
           inherit alejandra system;
-          username = "kevin";
+          username = user;
         };
       };
-
-      "kuni" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        modules = [
-          nix-index-database.hmModules.nix-index
-          ./nix/home.nix
-          ./nix/categories.nix
-          {
-            config = {
-              categories.enableAll = true;
-              gaming.enableAll = false;
-              home.withNixGL = rec {
-                enable = true;
-                package = pkgs.nixgl.nixGLIntel;
-                command = "${package}/bin/nixGLIntel";
-              };
-              wezterm.overrides = {
-                font_size = 9.5;
-              };
-              git = {inherit (git) userName userEmail;};
-              jj = {inherit (git) userName userEmail;};
-              myscripts.mrandr = {
-                SCREEN = "eDP";
-                OUTPUT = "DisplayPort-0";
-              };
+    }; #  }}}
+  in {
+    homeConfigurations = pkgs.lib.mergeAttrsList [
+      (makeUser "kevin" [
+        {
+          config = {
+            categories.enableAll = true;
+            mpv.withAnime4k = true;
+            wezterm.overrides = {
+              window_background_opacity = 0.97;
+              font_size = 11.1;
             };
-          }
-        ];
-
-        extraSpecialArgs = {
-          inherit alejandra system;
-          username = "kuni";
-        };
-      };
-
-      "ubuntu" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        modules = [
-          nix-index-database.hmModules.nix-index
-          ./nix/home.nix
-          ./nix/categories.nix
-          {
-            config = {
-              coding.enableAll = true;
-              handy-tools.enableAll = true;
-              system.enableAll = true;
-              terminal.enableAll = true;
-              latex.enable = true;
-              git = {inherit (git) userName userEmail;};
-              jj = {inherit (git) userName userEmail;};
+            git = {inherit (git) userName userEmail;};
+            jj = {inherit (git) userName userEmail;};
+            myscripts.mrandr = {
+              SCREEN = "eDP-2";
+              OUTPUT = "DP-1";
             };
-          }
-        ];
+          };
+        }
+      ])
 
-        extraSpecialArgs = {
-          inherit alejandra system;
-          username = "ubuntu";
-        };
-      };
-    };
+      (makeUser "kuni" [
+        {
+          config = {
+            categories.enableAll = true;
+            gaming.enableAll = false;
+            home.withNixGL = rec {
+              enable = true;
+              package = pkgs.nixgl.nixGLIntel;
+              command = "${package}/bin/nixGLIntel";
+            };
+            wezterm.overrides = {
+              font_size = 9.5;
+            };
+            git = {inherit (git) userName userEmail;};
+            jj = {inherit (git) userName userEmail;};
+            myscripts.mrandr = {
+              SCREEN = "eDP";
+              OUTPUT = "DisplayPort-0";
+            };
+          };
+        }
+      ])
+
+      (makeUser "ubuntu" [
+        {
+          config = {
+            coding.enableAll = true;
+            handy-tools.enableAll = true;
+            system.enableAll = true;
+            terminal.enableAll = true;
+            latex.enable = true;
+            git = {inherit (git) userName userEmail;};
+            jj = {inherit (git) userName userEmail;};
+          };
+        }
+      ])
+    ];
 
     devShells.${system}.default = pkgs.mkShell {
       name = "home-manager";
@@ -152,3 +135,5 @@
     };
   };
 }
+# vim: fdm=marker
+
