20213d31ecb16418185f9135f39445e2adcb8d39
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-03-27 00:16:45 +0100

    fix(ashell): Wrap using nixGL

--- nix/window-managers/ashell.nix
+++ nix/window-managers/ashell.nix
@@ -8,9 +8,20 @@
 in {
   options.ashell = {
     enable = lib.mkEnableOption "ashell";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default =
+        if config.home.withNixGL.enable
+        then
+          pkgs.writeShellScriptBin "ashell" ''
+            ${config.home.withNixGL.command} ${pkgs.ashell}/bin/ashell
+          ''
+        else pkgs.ashell;
+      description = "Package to use; wrap with nixGL if enabled";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [pkgs.ashell];
+    home.packages = [cfg.package];
   };
 }
