6ed0e024ab43e938075378f78999c844dc0e5c5e
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-03-27 00:39:50 +0100

    feat(ashell): Add configuration

--- nix/window-managers/ashell.nix
+++ nix/window-managers/ashell.nix
@@ -23,5 +23,30 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = [cfg.package];
+    fuzzel.enable = true;
+    wezterm.enable = true;
+    xdg.configFile."ashell.yml".text = builtins.toJSON {
+      appLauncherCmd = "fuzzel";
+      clipboardCmd = "${config.wezterm.package}/bin/wezterm start --class clipse -e clipse";
+      modules = {
+        left = [
+          "AppLauncher"
+          "Clipboard"
+          "Workspaces"
+        ];
+        center = [
+          # "WindowTitle"
+          "Clock"
+        ];
+        right = [
+          "Tray"
+          [
+            "Privacy"
+            "Settings"
+            "SystemInfo"
+          ]
+        ];
+      };
+    };
   };
 }
