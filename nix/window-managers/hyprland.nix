4230b32abe8caa36bfbbdaa65997f593665151a6
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-03-27 20:34:09 +0100

    feat(hyprland): Reduce gap sizes

--- nix/window-managers/hyprland.nix
+++ nix/window-managers/hyprland.nix
@@ -150,11 +150,13 @@ in {
             };
 
             #  {{{3
-            general = {
+            general = let
+              gap_size = 10;
+            in {
               # See https://wiki.hyprland.org/Configuring/Variables/ for more
 
-              gaps_in = 5;
-              gaps_out = 20;
+              gaps_in = gap_size / 2;
+              gaps_out = gap_size;
               border_size = 2;
               # col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg"; # ISSUE: Option does not exist anymore
               # col.inactive_border = "rgba(595959aa)"; # ISSUE: Option does not exist anymore
