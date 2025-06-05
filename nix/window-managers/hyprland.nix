21e262657fa6afdfb9ec0f10af4c3e30021c3607
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-05 12:17:29 +0200

    feat(hyprland): Enable hyprpolkitagent

--- nix/window-managers/hyprland.nix
+++ nix/window-managers/hyprland.nix
@@ -122,6 +122,8 @@ in {
         '')
       ];
 
+      services.hyprpolkitagent.enable = true;
+
       programs.kitty.enable = true; # required for the default Hyprland config
       wayland.windowManager.hyprland = {
         enable = true; # enable Hyprland
