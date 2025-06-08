52761ee52fc20e5ee1680d0561fca37537cd2b41
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-08 13:56:13 +0200

    chore(hours): Has been added to nixpkgs

--- nix/handy-tools/hours.nix
+++ nix/handy-tools/hours.nix
@@ -5,51 +5,12 @@
   ...
 }: let
   cfg = config.hours;
-  module = {
-    lib,
-    makeWrapper,
-    buildGoModule,
-    fetchFromGitHub,
-    dbPath ? "~/hours.db",
-  }:
-    buildGoModule rec {
-      pname = "hours";
-      version = "0.5.0";
-
-      src = fetchFromGitHub {
-        owner = "dhth";
-        repo = "hours";
-        tag = "v${version}";
-        sha256 = "sha256-B9M02THTCrr7ylbbflpkpTFMuoIwV2O0PQKOKbyxYPg=";
-      };
-
-      vendorHash = "sha256-5lhn0iTLmXUsaedvtyaL3qWLosmQaQVq5StMDl7pXXI=";
-
-      buildInputs = [makeWrapper];
-
-      postFixup = lib.strings.concatStringsSep " " [
-        "wrapProgram $out/bin/${pname}"
-        ''--append-flags --dbpath="${dbPath}"''
-      ];
-
-      meta = with lib; {
-        description = "A no-frills time tracking toolkit for command line nerds";
-        homepage = "https://tools.dhruvs.space/hours/";
-        license = licenses.mit;
-      };
-    };
 in {
   options.hours = {
     enable = lib.mkEnableOption "hours";
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.callPackage module {
-        dbPath = "${config.xdg.cacheHome}/hours/hours.db";
-      };
-    };
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [cfg.package];
+    home.packages = [pkgs.hours];
   };
 }
