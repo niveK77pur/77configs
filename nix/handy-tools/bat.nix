5c2b1898e8df71fcb7b65506a28c4314a46f1f25
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-10 14:49:54 +0200

    feat(bat): Use bat as a MANPAGER

--- nix/handy-tools/bat.nix
+++ nix/handy-tools/bat.nix
@@ -1,10 +1,41 @@
 {
+  pkgs,
   lib,
   config,
   ...
-}: {
-  options.bat.enable = lib.mkEnableOption "bat";
-  config = lib.mkIf config.bat.enable {
-    programs.bat.enable = true;
+}: let
+  cfg = config.bat;
+in {
+  options.bat = {
+    enable = lib.mkEnableOption "bat";
+
+    enableBashIntegration = lib.hm.shell.mkBashIntegrationOption {
+      inherit config;
+      extraDescription = "If enabled, this will bind `ctrl-r` to open the Atuin history.";
+    };
+
+    enableFishIntegration = lib.hm.shell.mkFishIntegrationOption {
+      inherit config;
+      extraDescription = "If enabled, this will bind the up-arrow key to open the Atuin history.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs = {
+      bat = {
+        enable = true;
+        extraPackages = with pkgs.bat-extras; [
+          batman
+        ];
+      };
+
+      fish.interactiveShellInit = lib.mkIf cfg.enableFishIntegration ''
+        batman --export-env | source
+      '';
+
+      bash.initExtra = lib.mkIf cfg.enableBashIntegration ''
+        eval "$(batman --export-env)"
+      '';
+    };
   };
 }
