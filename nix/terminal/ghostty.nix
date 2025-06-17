a0edd4821fca56c7b52fd6c315ef659e33c0831f
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-17 16:10:50 +0200

    fix(ghostty): Wrap with NixGL for launching

--- nix/terminal/ghostty.nix
+++ nix/terminal/ghostty.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -7,10 +8,21 @@
 in {
   options.ghostty = {
     enable = lib.mkEnableOption "ghostty";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default =
+        if config.home.withNixGL.enable
+        then
+          pkgs.writeShellScriptBin "ghostty" ''
+            ${config.home.withNixGL.command} ${pkgs.ghostty}/bin/ghostty
+          ''
+        else pkgs.ghostty;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     programs.ghostty = {
+      inherit (cfg) package;
       enable = true;
       installVimSyntax = true;
       settings = {
