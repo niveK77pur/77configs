68a030c805366d89917b55b722f257c1d1565b4b
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-06-24 12:12:45 +0200

    feat(wlogout): Function to read layout file into nix attrset

--- nix/window-managers/wlogout.nix
+++ nix/window-managers/wlogout.nix
@@ -5,6 +5,38 @@
   ...
 }: let
   cfg = config.wlogout;
+  #  {{{1
+  fromWlogoutLayout = layoutFile:
+  # Each JSON directory will be transformed so that the 'text' attribute
+  # becomes the key of the attribute set. This makes it simpler to modify the
+  # data in nix as opposed to using a list of attribute sets.
+  # i.e:
+  #      [..., { ..., text = "MyButton", ... }, ...]
+  #   => { ..., "MyButton" = { ... }, ... }
+    lib.lists.foldr
+    (e: agg: agg // {"${e.text}" = builtins.removeAttrs e ["text"];})
+    {}
+    (
+      builtins.fromJSON (
+        lib.strings.concatStrings [
+          "["
+          (
+            builtins.concatStringsSep "," (
+              # retrieve the individual dict entries
+              lib.lists.flatten (
+                builtins.filter (e: (builtins.typeOf e) == "list") (
+                  builtins.split ''(\{[^}]*})'' (
+                    builtins.readFile layoutFile
+                  )
+                )
+              )
+            )
+          )
+          "]"
+        ]
+      )
+    );
+  #  }}}1
 in {
   options.wlogout = {
     enable = lib.mkEnableOption "wlogout";
