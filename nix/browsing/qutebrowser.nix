cecd179a551681cbcf25808fae568471fce4a250
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-07-07 19:37:43 +0200

    fix(qutebrowser): Wrap with nixGL

--- nix/browsing/qutebrowser.nix
+++ nix/browsing/qutebrowser.nix
@@ -1,16 +1,25 @@
 {
   lib,
   config,
+  wrapNixGL,
   ...
 }: let
   cfg = config.qutebrowser;
 in {
   options.qutebrowser = {
     enable = lib.mkEnableOption "qutebrowser";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = wrapNixGL {
+        binName = "qutebrowser";
+        inherit config;
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
     programs.qutebrowser = {
+      inherit (cfg) package;
       enable = true;
       settings = {};
     };
