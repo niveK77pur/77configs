7f39104bf848d861b84554a118832ad7d8145eec
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-07-15 13:00:22 +0200

    feat(autoraise): Add launchd service

--- nix/window-managers/autoraise.nix
+++ nix/window-managers/autoraise.nix
@@ -8,9 +8,44 @@
 in {
   options.autoraise = {
     enable = lib.mkEnableOption "autoraise";
+    service = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          enable = lib.mkEnableOption "autoraise-launchd";
+          stdOutputPath = lib.mkOption {
+            type = lib.types.nullOr lib.types.path;
+            default = null;
+            description = "Which path to put stdout and stderr into, if any";
+          };
+        };
+      };
+      default = {
+        enable = true;
+      };
+      description = "Enable the autoraise launchd service";
+    };
   };
 
-  config = lib.mkIf cfg.enable {
-    home.packages = [pkgs.autoraise];
-  };
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      home.packages = [pkgs.autoraise];
+    }
+    (lib.mkIf cfg.service.enable {
+      launchd.agents.autoraise = {
+        enable = true;
+        config = lib.mkMerge [
+          {
+            ProgramArguments = ["${pkgs.autoraise}/bin/autoraise"];
+            RunAtLoad = true;
+            KeepAlive = true;
+            ProcessType = "Interactive";
+          }
+          (lib.mkIf (cfg.service.stdOutputPath != null) {
+            StandardOutPath = "${cfg.service.stdOutputPath}/autoraise.stdout";
+            StandardErrorPath = "${cfg.service.stdOutputPath}/autoraise.stderr";
+          })
+        ];
+      };
+    })
+  ]);
 }
