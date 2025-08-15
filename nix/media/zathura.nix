44467fe5f8dd1988670e706964258849fb0df448
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-08-15 15:17:42 +0100

    refactor(zathura): Use a lib.mkMerge

--- nix/media/zathura.nix
+++ nix/media/zathura.nix
@@ -4,45 +4,47 @@
   ...
 }: {
   options.zathura.enable = lib.mkEnableOption "zathura";
-  config = lib.mkIf config.zathura.enable {
-    programs.zathura = {
-      enable = true;
-      options = lib.mergeAttrsList [
-        {
-          "zoom-step" = 20;
-          "selection-clipboard" = "clipboard";
-          "window-title-home-tilde" = true;
-          "statusbar-basename" = true;
+  config = lib.mkIf config.zathura.enable (lib.mkMerge [
+    {
+      programs.zathura = {
+        enable = true;
+        options = lib.mergeAttrsList [
+          {
+            "zoom-step" = 20;
+            "selection-clipboard" = "clipboard";
+            "window-title-home-tilde" = true;
+            "statusbar-basename" = true;
 
-          # Recoloring
-          "recolor-reverse-video" = true;
-          "recolor-keephue" = true;
-          "recolor" = true;
-        }
+            # Recoloring
+            "recolor-reverse-video" = true;
+            "recolor-keephue" = true;
+            "recolor" = true;
+          }
 
-        (lib.optionalAttrs (!config.sx.enable) {
-          "recolor-darkcolor" = "#ced5e5";
-          "recolor-lightcolor" = "#201f30";
-          # "recolor-darkcolor" = "#d8caac";
-          # "recolor-lightcolor" = "#323d43";
-          # "default-bg" = "#2c343a";
-          # "default-bg" = "#262E33";
-          # "default-bg" = "#222A2E";
-        })
-      ];
-      mappings = {
-        "[normal] <Space>" = "navigate next";
-        "[normal] <BackSpace>" = "navigate previous";
-        "[fullscreen] <Space>" = "navigate next";
-        "[fullscreen] <BackSpace>" = "navigate previous";
+          (lib.optionalAttrs (!config.sx.enable) {
+            "recolor-darkcolor" = "#ced5e5";
+            "recolor-lightcolor" = "#201f30";
+            # "recolor-darkcolor" = "#d8caac";
+            # "recolor-lightcolor" = "#323d43";
+            # "default-bg" = "#2c343a";
+            # "default-bg" = "#262E33";
+            # "default-bg" = "#222A2E";
+          })
+        ];
+        mappings = {
+          "[normal] <Space>" = "navigate next";
+          "[normal] <BackSpace>" = "navigate previous";
+          "[fullscreen] <Space>" = "navigate next";
+          "[fullscreen] <BackSpace>" = "navigate previous";
 
-        # Does not seem to work
-        "[normal] <S-b>" = "mark_add m";
-        "[normal] b" = "mark_evaluate m";
+          # Does not seem to work
+          "[normal] <S-b>" = "mark_add m";
+          "[normal] b" = "mark_evaluate m";
+        };
+        extraConfig = lib.concatLines [
+          "unmap [normal] <C-p>"
+        ];
       };
-      extraConfig = lib.concatLines [
-        "unmap [normal] <C-p>"
-      ];
-    };
-  };
+    }
+  ]);
 }
