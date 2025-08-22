441d6e69cfccf1d2ac44d4405711599dbb253e10
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-08-22 15:55:21 +0100

    refactor(fish): Use concatLines for the shellInit

--- nix/terminal/fish.nix
+++ nix/terminal/fish.nix
@@ -19,10 +19,12 @@
       programs.fish = {
         enable = true;
 
-        shellInit = ''
-          # lfcd
-          bind \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
-        '';
+        shellInit = lib.concatLines [
+          ''
+            # lfcd
+            bind \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
+          ''
+        ];
 
         functions = lib.mkMerge [
           {
