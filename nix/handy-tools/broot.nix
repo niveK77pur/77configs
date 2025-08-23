f4b3b8f290ca73d38f4f555b48f5a3c7fd227055
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-08-23 15:12:40 +0100

    feat(broot): Define generic `open` verb

--- nix/handy-tools/broot.nix
+++ nix/handy-tools/broot.nix
@@ -12,6 +12,14 @@ in {
   config = lib.mkIf cfg.enable {
     programs.broot = {
       enable = true;
+      settings = {
+        verbs = [
+          {
+            invocation = "open {command}";
+            execution = "{command} {file}";
+          }
+        ];
+      };
     };
   };
 }
