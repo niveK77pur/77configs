a6cae8499ca097a36bc3c8a033a72b11fa1766a2
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-10-09 11:02:52 +0200

    feat(gcloud): Helper fish function to ssh into machines

--- nix/coding/gcloud.nix
+++ nix/coding/gcloud.nix
@@ -12,5 +12,18 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = [pkgs.google-cloud-sdk];
+    programs.fish.functions = let
+      gcloud = "${pkgs.google-cloud-sdk}/bin/gcloud";
+    in
+      lib.mergeAttrsList [
+        {
+          gssh = {
+            body = "${gcloud} compute ssh $host -- -A";
+            argumentNames = "host";
+            wraps = gcloud;
+            description = "Easily ssh into gcloud machines";
+          };
+        }
+      ];
   };
 }
