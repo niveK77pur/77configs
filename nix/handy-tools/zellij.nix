63e38035b50ba895cafd1b8aab982e638407b849
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-10-31 13:54:22 +0000

    feat(zellij): Update using default config with "Unlock-First (non-colliding)"
    See: https://zellij.dev/tutorials/colliding-keybindings/

--- nix/handy-tools/zellij.nix
+++ nix/handy-tools/zellij.nix
@@ -1,15 +1,15 @@
 {
-  pkgs,
   lib,
   config,
   ...
 }: {
   options.zellij.enable = lib.mkEnableOption "zellij";
   config = lib.mkIf config.zellij.enable {
-    home.packages = [pkgs.zellij];
-    xdg.configFile.zellij = {
-      source = ../../config/zellij;
-      recursive = true;
+    programs.zellij = {
+      enable = true;
+    };
+    xdg.configFile."zellij/config.kdl" = {
+      source = ../../config/zellij/config.kdl;
     };
   };
 }
