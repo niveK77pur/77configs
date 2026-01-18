29f36e7b5389e0c2fa353a2a6f553df0d1b2babf
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-01-18 15:56:27 +0100

    fix(neovim): nvim-ghost plugin uses custom python binary
    See: https://github.com/subnut/nvim-ghost.nvim/issues/66

--- nix/coding/neovim.nix
+++ nix/coding/neovim.nix
@@ -4,9 +4,34 @@
   lib,
   helper,
   ...
-}: {
+}: let
+  python-ghosttext-dependencies = pkgs.python3.withPackages (ps:
+    with ps; [
+      pynvim
+      requests
+      # nixpkgs has a different version of simple-websocket-server than on pypi
+      (buildPythonPackage {
+        pname = "simple-websocket-server";
+        version = "0.4.4";
+        format = "setuptools";
+        src = pkgs.fetchFromGitHub {
+          owner = "pikhovkin";
+          repo = "simple-websocket-server";
+          rev = "47a7dce556d22db483ec9c3db72b6c87736d8063";
+          sha256 = "sha256-FlrYdT2wH6dj/72ELhfzwvfuoinhMy4/wEMEiQQp/ro=";
+        };
+        meta = with lib; {
+          description = "A simple WebSocket server";
+          homepage = "https://github.com/pikhovkin/simple-websocket-server";
+          license = licenses.mit;
+          platforms = platforms.all;
+        };
+      })
+    ]);
+in {
   options.neovim.enable = lib.mkEnableOption "neovim";
   config = lib.mkIf config.neovim.enable {
+    home.sessionVariables.NVIM_GHOST_PYTHON_EXECUTABLE = lib.getExe python-ghosttext-dependencies;
     programs = {
       neovim = {
         enable = true;
