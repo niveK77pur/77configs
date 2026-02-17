3e144ff9712211e0a11abf187220802b0c28ff24
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-02-17 18:01:35 +0100

    feat(glab): Helper fzf script to create MR by selecting source/dest

--- nix/coding/glab.nix
+++ nix/coding/glab.nix
@@ -5,12 +5,35 @@
   ...
 }: let
   cfg = config.glab;
+  glab-mr-fzf = pkgs.writers.writeFishBin "glab-mr-fzf" ''
+    function jj-bookmark-select -a prompt
+      set -f bookmark (jj bookmark list \
+        --all \
+        --template 'join(":", self.name(), self.remote(), if(self.conflict(), "[conflict]", ""), if(self.synced(), "", "[out of sync]")) ++ "\n"' \
+        | ${lib.getExe pkgs.fzf} \
+            --prompt "$prompt> " \
+            --height 10 \
+            --delimiter ':' \
+            --with-nth '{1} [remote: @{2}]{3}{4}' \
+            --accept-nth '{1}:{2}' \
+        | string split :)
+      if [ -z $bookmark[2] ]
+        echo $bookmark[1]
+      else
+        echo $bookmark[1]@$bookmark[2]
+      end
+    end
+    ${lib.getExe pkgs.glab} mr create -s (jj-bookmark-select source) -b (jj-bookmark-select destination)
+  '';
 in {
   options.glab = {
     enable = lib.mkEnableOption "glab";
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [pkgs.glab];
+    home.packages = [
+      pkgs.glab
+      glab-mr-fzf
+    ];
   };
 }
