f2d96d9a66a7d3a0d3b41851e9e845776c3cfa25
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-02-18 10:41:42 +0100

    feat(glab): Allow passing default query to internal glab-mr-fzf

--- nix/coding/glab.nix
+++ nix/coding/glab.nix
@@ -6,13 +6,14 @@
 }: let
   cfg = config.glab;
   glab-mr-fzf = pkgs.writers.writeFishBin "glab-mr-fzf" ''
-    function jj-bookmark-select -a prompt
+    function jj-bookmark-select -a prompt query
       set -f bookmark (jj bookmark list \
         --all \
         --template 'join(":", self.name(), self.remote(), if(self.conflict(), "[conflict]", ""), if(self.synced(), "", "[out of sync]")) ++ "\n"' \
         | ${lib.getExe pkgs.fzf} \
             --prompt "$prompt> " \
             --height 10 \
+            --query "$query" \
             --delimiter ':' \
             --with-nth '{1} [remote: @{2}]{3}{4}' \
             --accept-nth '{1}:{2}' \
@@ -23,7 +24,7 @@
         echo $bookmark[1]@$bookmark[2]
       end
     end
-    ${lib.getExe pkgs.glab} mr create -s (jj-bookmark-select source) -b (jj-bookmark-select destination)
+    ${lib.getExe pkgs.glab} mr create -s (jj-bookmark-select source) -b (jj-bookmark-select destination "'dev | 'main")
   '';
 in {
   options.glab = {
