6c9059cd7c2bb48ae03b9e7c5a15f82401cdb933
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-02-19 13:34:38 +0100

    feat(parallel): Add shell completions

--- nix/handy-tools/parallel.nix
+++ nix/handy-tools/parallel.nix
@@ -3,10 +3,20 @@
   config,
   lib,
   ...
-}: {
-  options.parallel.enable = lib.mkEnableOption "parallel";
+}: let
+  cfg = config.parallel;
+in {
+  options.parallel = {
+    enable = lib.mkEnableOption "parallel";
+    enableBashIntegration = lib.hm.shell.mkBashIntegrationOption {inherit config;};
+    enableZshIntegration = lib.hm.shell.mkZshIntegrationOption {inherit config;};
+  };
 
   config = lib.mkIf config.parallel.enable {
     home.packages = [pkgs.parallel];
+    programs = {
+      bash.initExtra = lib.mkIf cfg.enableBashIntegration ''eval "$(parallel --shell-completion bash)"'';
+      zsh.initContent = lib.mkIf cfg.enableZshIntegration ''eval "$(parallel --shell-completion zsh)"'';
+    };
   };
 }
