bbdf7f46a52def2d5b61f92f77e6b02b0cf3b5bd
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-03-13 09:16:00 +0100

    fix(glab): FZF mr helper outputs branch name without origin

--- nix/coding/glab.nix
+++ nix/coding/glab.nix
@@ -7,7 +7,7 @@
   cfg = config.glab;
   glab-mr-fzf = pkgs.writers.writeFishBin "glab-mr-fzf" ''
     function jj-bookmark-select -a prompt query
-      set -f bookmark (jj bookmark list \
+      jj bookmark list \
         --all \
         --template 'join(":", self.name(), self.remote(), if(self.conflict(), "[conflict]", ""), if(self.synced(), "", "[out of sync]")) ++ "\n"' \
         | ${lib.getExe pkgs.fzf} \
@@ -21,12 +21,7 @@
             --delimiter ':' \
             --with-nth '{1} [remote: @{2}]{3}{4}' \
             --accept-nth '{1}:{2}' \
-        | string split :)
-      if [ -z $bookmark[2] ]
-        echo $bookmark[1]
-      else
-        echo $bookmark[1]@$bookmark[2]
-      end
+        | string split --fields 1 :
     end
     ${lib.getExe pkgs.glab} mr create -s (jj-bookmark-select source) -b (jj-bookmark-select destination "'dev | 'main")
   '';
