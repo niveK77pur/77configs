3a25b0d476121b6efa04b3a106662dc69c55eeb9
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-03 20:04:19 +0200

    feat(rio): Update configuration

--- nix/terminal/rio.nix
+++ nix/terminal/rio.nix
@@ -5,11 +5,32 @@
 }: {
   options.rio.enable = lib.mkEnableOption "rio";
   config = lib.mkIf config.rio.enable {
-    
+    assertions = [
+      {
+        # See: https://rioterm.com/docs/config#navigationcurrent-working-directory
+        assertion = let
+          rs = config.programs.rio.settings;
+        in
+          ((rs ? navigation.current-working-directory) && rs.navigation.current-working-directory)
+          -> !((rs ? use-fork) -> rs.use-fork);
+        message = "navigation.current-working-directory=true requires use-fork=false";
+      }
+    ];
     programs.rio = {
       enable = true;
       settings = {
-        use-fork = true;
+        use-fork = false;
+        enable-scroll-bar = true;
+        effects = {
+          trail-cursor = true;
+        };
+        navigation = {
+          current-working-directory = true;
+        };
+        scroll = {
+          multiplier = 9.0;
+          divider = 1.0;
+        };
       };
     };
   };
