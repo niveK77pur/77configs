62f812f04be90d0f2185ab90fec842a66adc8b5f
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-07 11:05:36 +0200

    feat(hyprland): Set rio font size via stylix

--- nix/window-managers/hyprland.nix
+++ nix/window-managers/hyprland.nix
@@ -154,6 +154,7 @@ in {
         enable = true;
       };
       stylix.targets.ghostty.fonts.override.sizes.terminal = 10;
+      stylix.targets.rio.fonts.override.sizes.terminal = 10;
       wlogout = {
         enable = true;
         override-layout = {
