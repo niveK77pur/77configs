808bbc98ce8ed177571366d0181a6be73996b0c8
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-13 16:02:44 +0200

    refactor(wezterm): Use lib.mkMerge

--- nix/terminal/wezterm.nix
+++ nix/terminal/wezterm.nix
@@ -27,90 +27,93 @@ in {
     };
   };
 
-  config = lib.mkIf config.wezterm.enable {
-    programs.wezterm = {
-      enable = true;
-      inherit (cfg) package;
-      settings = {
-        # Interface {{{1
-        enable_tab_bar = true;
-        hide_tab_bar_if_only_one_tab = true;
-        inherit (cfg.overrides) window_background_opacity;
-        window_padding = let
-          padding = 10;
-        in {
-          left = padding;
-          right = padding;
-          top = padding;
-          bottom = padding;
-        };
-        # Behaviour {{{1
-        warn_about_missing_glyphs = false;
-        check_for_updates = false;
-        scrollback_lines = 3500;
-
-        # uses rust regex: https://docs.rs/regex/1.3.9/regex/#syntax
-        quick_select_patterns = [
-          ''\\b[[:alnum:][:punct:]]+\\b'' # "words"
-        ];
-        quick_select_alphabet = let
-          # suggested alphabet for keyboard layout
-          layouts = {
-            qwerty = "asdfqwerzxcvjklmiuopghtybn";
-            qwertz = "asdfqweryxcvjkluiopmghtzbn";
-            azerty = "qsdfazerwxcvjklmuiopghtybn";
-            dvorak = "aoeuqjkxpyhtnsgcrlmwvzfidb";
-            colemak = "arstqwfpzxcvneioluymdhgjbk";
+  config = lib.mkIf config.wezterm.enable (lib.mkMerge [
+    {
+      programs.wezterm = {
+        enable = true;
+        inherit (cfg) package;
+        settings = {
+          # Interface {{{1
+          enable_tab_bar = true;
+          hide_tab_bar_if_only_one_tab = true;
+          inherit (cfg.overrides) window_background_opacity;
+          window_padding = let
+            padding = 10;
+          in {
+            left = padding;
+            right = padding;
+            top = padding;
+            bottom = padding;
           };
-        in
-          layouts.qwertz;
-        audible_bell = "Disabled";
+          # Behaviour {{{1
+          warn_about_missing_glyphs = false;
+          check_for_updates = false;
+          scrollback_lines = 3500;
 
-        # {{{1
-        keys = [
-          {
-            # Interferes with jjui
-            key = "Enter";
-            mods = "ALT";
-            action = lib.generators.mkLuaInline "wezterm.action.DisableDefaultAssignment";
-          }
-          {
-            # Match ghostty default
-            key = "o";
-            mods = "CTRL|SHIFT";
-            action = lib.generators.mkLuaInline ''wezterm.action.SplitHorizontal {domain="CurrentPaneDomain"}'';
-          }
-          {
-            # Match ghostty default
-            key = "e";
-            mods = "CTRL|SHIFT";
-            action = lib.generators.mkLuaInline ''wezterm.action.SplitVertical {domain="CurrentPaneDomain"}'';
-          }
-          {
-            key = "h";
-            mods = "CTRL|SHIFT";
-            action = lib.generators.mkLuaInline ''wezterm.action.ActivatePaneDirection "Left"'';
-          }
-          {
-            key = "j";
-            mods = "CTRL|SHIFT";
-            action = lib.generators.mkLuaInline ''wezterm.action.ActivatePaneDirection "Down"'';
-          }
-          {
-            key = "k";
-            mods = "CTRL|SHIFT";
-            action = lib.generators.mkLuaInline ''wezterm.action.ActivatePaneDirection "Up"'';
-          }
-          {
-            key = "l";
-            mods = "CTRL|SHIFT";
-            action = lib.generators.mkLuaInline ''wezterm.action.ActivatePaneDirection "Right"'';
-          }
-        ];
-        #  }}}1
+          # uses rust regex: https://docs.rs/regex/1.3.9/regex/#syntax
+          quick_select_patterns = [
+            ''\\b[[:alnum:][:punct:]]+\\b'' # "words"
+          ];
+          quick_select_alphabet = let
+            # suggested alphabet for keyboard layout
+            layouts = {
+              qwerty = "asdfqwerzxcvjklmiuopghtybn";
+              qwertz = "asdfqweryxcvjkluiopmghtzbn";
+              azerty = "qsdfazerwxcvjklmuiopghtybn";
+              dvorak = "aoeuqjkxpyhtnsgcrlmwvzfidb";
+              colemak = "arstqwfpzxcvneioluymdhgjbk";
+            };
+          in
+            layouts.qwertz;
+          audible_bell = "Disabled";
+
+          # {{{1
+          keys = [
+            {
+              # Interferes with jjui
+              key = "Enter";
+              mods = "ALT";
+              action = lib.generators.mkLuaInline "wezterm.action.DisableDefaultAssignment";
+            }
+            {
+              # Match ghostty default
+              key = "o";
+              mods = "CTRL|SHIFT";
+              action = lib.generators.mkLuaInline ''wezterm.action.SplitHorizontal {domain="CurrentPaneDomain"}'';
+            }
+            {
+              # Match ghostty default
+              key = "e";
+              mods = "CTRL|SHIFT";
+              action = lib.generators.mkLuaInline ''wezterm.action.SplitVertical {domain="CurrentPaneDomain"}'';
+            }
+            {
+              key = "h";
+              mods = "CTRL|SHIFT";
+              action = lib.generators.mkLuaInline ''wezterm.action.ActivatePaneDirection "Left"'';
+            }
+            {
+              key = "j";
+              mods = "CTRL|SHIFT";
+              action = lib.generators.mkLuaInline ''wezterm.action.ActivatePaneDirection "Down"'';
+            }
+            {
+              key = "k";
+              mods = "CTRL|SHIFT";
+              action = lib.generators.mkLuaInline ''wezterm.action.ActivatePaneDirection "Up"'';
+            }
+            {
+              key = "l";
+              mods = "CTRL|SHIFT";
+              action = lib.generators.mkLuaInline ''wezterm.action.ActivatePaneDirection "Right"'';
+            }
+          ];
+          #  }}}1
+        };
       };
-    };
-  };
+    }
+    }
+  ]);
 }
 # vim: fdm=marker
 
