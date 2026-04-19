80a8bb40e126c4e51ff42dc5f695cfc3dd7ca059
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-04-19 16:42:12 +0200

    feat(sioyek): Add configuration

--- nix/media/sioyek.nix
+++ nix/media/sioyek.nix
@@ -12,6 +12,13 @@ in {
   config = lib.mkIf cfg.enable {
     programs.sioyek = {
       enable = true;
+      config = {
+        should_launch_new_window = "yes";
+        should_warn_about_user_key_override = "yes";
+        should_draw_unrendered_pages = "yes";
+        show_closest_bookmark_in_statusbar = "yes";
+        show_close_portal_in_statusbar = "yes";
+      };
     };
   };
 }
