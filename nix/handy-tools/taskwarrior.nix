2c1b103ebaa5a37d27c63aeec29bd8ccc62ea3e7
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-05-15 20:18:43 +0200

    chore(taskwarrior): Remove useless module option

--- nix/handy-tools/taskwarrior.nix
+++ nix/handy-tools/taskwarrior.nix
@@ -8,17 +8,12 @@
 in {
   options.taskwarrior = {
     enable = lib.mkEnableOption "taskwarrior";
-    extraConfig = lib.mkOption {
-      type = lib.types.attrsOf lib.types.anything;
-      default = {};
-    };
   };
 
   config = lib.mkIf cfg.enable {
     programs.taskwarrior = {
       enable = true;
       package = pkgs.taskwarrior3;
-      config = {} // cfg.extraConfig;
     };
 
     home.packages = [pkgs.taskwarrior-tui];
