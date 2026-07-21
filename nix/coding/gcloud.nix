3369fcb726e12ddc5453bb98889478bef4493857
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-07-21 12:54:42 +0200

    fix(gcloud): Disable from topgrade if installed via home-manager

--- nix/coding/gcloud.nix
+++ nix/coding/gcloud.nix
@@ -9,36 +9,40 @@
 in {
   options.gcloud = {
     enable = lib.mkEnableOption "gcloud";
+    disableTopgrade = lib.mkEnableOption "gcloud-topgrade" // {default = cfg.enable;};
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = [pkgs.google-cloud-sdk];
-    programs.fish.functions = let
-      gcloud = "${pkgs.google-cloud-sdk}/bin/gcloud";
-    in
-      lib.mergeAttrsList [
-        {
-          gssh = {
-            body = "${gcloud} compute ssh $host -- -A";
-            argumentNames = "host";
-            wraps = gcloud;
-            description = "Easily ssh into gcloud machines";
-          };
+    programs = {
+      fish.functions = let
+        gcloud = "${pkgs.google-cloud-sdk}/bin/gcloud";
+      in
+        lib.mergeAttrsList [
+          {
+            gssh = {
+              body = "${gcloud} compute ssh $host -- -A";
+              argumentNames = "host";
+              wraps = gcloud;
+              description = "Easily ssh into gcloud machines";
+            };
 
-          glist = helper.makeFishAliasFunction {
-            body = "${gcloud} compute instances list $argv";
-          };
-        }
+            glist = helper.makeFishAliasFunction {
+              body = "${gcloud} compute instances list $argv";
+            };
+          }
 
-        (lib.optionalAttrs config.programs.ghostty.enable {
-          gghostty = let
-            infocmp = "${pkgs.ncurses}/bin/infocmp";
-          in {
-            body = "${infocmp} -x xterm-ghostty | ${gcloud} compute ssh $host -- tic -x -";
-            argumentNames = "host";
-            description = "Easily copy ghostty terminfo to gcloud machine";
-          };
-        })
-      ];
+          (lib.optionalAttrs config.programs.ghostty.enable {
+            gghostty = let
+              infocmp = "${pkgs.ncurses}/bin/infocmp";
+            in {
+              body = "${infocmp} -x xterm-ghostty | ${gcloud} compute ssh $host -- tic -x -";
+              argumentNames = "host";
+              description = "Easily copy ghostty terminfo to gcloud machine";
+            };
+          })
+        ];
+      topgrade.settings.misc.disable = lib.optional cfg.disableTopgrade "gcloud";
+    };
   };
 }
